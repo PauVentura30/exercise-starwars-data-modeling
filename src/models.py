f240f0e0fa88a41d0103f32ec89f64e04acec3c5
PauVentura30 <pauventuraa@gmail.com>
2025-02-23 13:07:55 +0000

    final

--- src/models.py
+++ src/models.py
@@ -1,33 +1,80 @@
 import os
 import sys
-from sqlalchemy.orm import declarative_base, Mapped, mapped_column, relationship
-from sqlalchemy import create_engine, String, ForeignKey
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
+from sqlalchemy.orm import relationship, declarative_base, ForeignKey, Table, Column
+from sqlalchemy import create_engine
 from eralchemy2 import render_er
+from datetime import datetime, timezone
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(nullable=False)
-    address: Mapped["Address"] = relationship(back_populates="person")
+class User(Base):
+    __tablename__ = 'user'
+    
+    id = Column(Integer, primary_key=True)
+    username = Column(String(40), nullable=False, unique=True)
+    password = Column(String(40), nullable=False)
+    full_name = Column(String(200), nullable=False)
+    email = Column(String(100), nullable=False, unique=True)
+    created = Column(DateTime, default=lambda: datetime.now(timezone.utc), nullable=False)
+    
+    favorites = relationship('Favorite', backref='user', lazy=True)
 
+class Planet(Base):
+    __tablename__ = 'planet'
+    
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), nullable=False)
+    diameter = Column(String(100), nullable=False)
+    rotation_period = Column(String(100), nullable=False)
+    orbital_period = Column(String(100), nullable=False)
+    gravity = Column(String(100), nullable=False)
+    population = Column(String(100), nullable=False)
+    climate = Column(String(100), nullable=False)
+    terrain = Column(String(100), nullable=False)
+    surface_water = Column(String(100), nullable=False)
+    created = Column(DateTime, default=lambda: datetime.now(timezone.utc), nullable=False)
+    edited = Column(DateTime, default=lambda: datetime.now(timezone.utc), onupdate=lambda: datetime.now(timezone.utc))
+    
+    favorites = relationship('Favorite', backref='planet', lazy=True)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id: Mapped[int] = mapped_column(primary_key=True)
-    street_name: Mapped[str]
-    street_number: Mapped[str]
-    post_code: Mapped[str] = mapped_column(nullable=False)
-    person_id: Mapped[int] = mapped_column(ForeignKey("person.id"))
-    person: Mapped["Person"] = relationship(back_populates="address")
+class Vehicle(Base):
+    __tablename__ = 'vehicle'
+    
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), nullable=False)
+    model = Column(String(100), nullable=False)
+    vehicle_class = Column(String(100), nullable=False)
+    cost_in_credits = Column(String(100), nullable=False)
+    max_atmosphering_speed = Column(String(100), nullable=False)
+    cargo_capacity = Column(String(100), nullable=False)
+    url = Column(String(200), nullable=False)
+    created = Column(DateTime, default=lambda: datetime.now(timezone.utc), nullable=False)
+    edited = Column(DateTime, default=lambda: datetime.now(timezone.utc), onupdate=lambda: datetime.now(timezone.utc))
+    
+    favorites = relationship('Favorite', backref='vehicle', lazy=True)
 
-    def to_dict(self):
-        return {}
+class People(Base):
+    __tablename__ = 'people'
+    
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), nullable=False)
+    birth_year = Column(String(100), nullable=False)
+    gender = Column(String(100), nullable=False)
+    homeworld = Column(String(40), nullable=False)
+    url = Column(String(100), nullable=False)
+    created = Column(DateTime, default=lambda: datetime.now(timezone.utc), nullable=False)
+    edited = Column(DateTime, default=lambda: datetime.now(timezone.utc), onupdate=lambda: datetime.now(timezone.utc))
+    
+    favorites = relationship('Favorite', backref='people', lazy=True)
 
-## Draw from SQLAlchemy base
+class Favorite(Base):
+    __tablename__ = 'favorite'
+    
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    people_id = Column(Integer, ForeignKey('people.id'), nullable=True)
+    vehicle_id = Column(Integer, ForeignKey('vehicle.id'), nullable=True)
+    planet_id = Column(Integer, ForeignKey('planet.id'), nullable=True)
+
 render_er(Base, 'diagram.png')
